ca3f15e1cf5eb27630bd77778d4a6ad73870e0e7
Sean Nelson <audiohacked@gmail.com>
2014-06-10 03:01:49 -0600

    update main.cpp for Fan Support patch 5

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char **argv) {
 					fprintf(stdout, "Setting fan RPM to %i\n", fanRPM);
 					cl->fans->fanInfo[fanNumber].RPM = fanRPM;
 				}
-				cl->fans->SetFansInfo(fanNumber - 1, fanInfo);
+				cl->fans->SetFansInfo(fanNumber - 1, cl->fans->fanInfo[i]);
 			}
 		} else {
 			fprintf(stdout, "No mode or fan RPM specified for the fan.\n");
