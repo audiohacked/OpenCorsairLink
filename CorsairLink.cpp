c9193c17289f0bba770cbb618239909c1db9e6c7
Sean Nelson <audiohacked@gmail.com>
2014-07-14 07:29:41 -0600

    add License Info

--- CorsairLink.cpp
+++ CorsairLink.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of OpenCorsairLink.
+ * Copyright (C) 2014  Sean Nelson <audiohacked@gmail.com>
+
+ * OpenCorsairLink is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+
+ * OpenCorsairLink is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
