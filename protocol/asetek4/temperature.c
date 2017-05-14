2868b76f4612620fac9678bc0944876445f84b6d
Li Yin <liyin@live.com>
2017-05-14 14:31:21 +0800

    Fix wrong index

--- protocol/asetek4/temperature.c
+++ protocol/asetek4/temperature.c
@@ -53,7 +53,7 @@ int corsairlink_asetek_temperature(struct corsair_device_info *dev, struct libus
 	commands[15] = 0x00;
 	commands[16] = 0x01;
 	commands[17] = 0x00;
-	commands[19] = 0x01;
+	commands[18] = 0x01;
 
 	r = dev->driver->write(handle, dev->write_endpoint, commands, 19);
 	r = dev->driver->read(handle, dev->read_endpoint, response, 32);
